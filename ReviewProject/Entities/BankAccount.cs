809b8210f78100b2e5de82a7a44da5bd84345c69
bhp <bhp@easv.dk>
2019-10-27 22:06:29 +0100

    Implemented BankAccount with BankAccountTest

--- ReviewProject/Entities/BankAccount.cs
+++ ReviewProject/Entities/BankAccount.cs
@@ -8,31 +8,55 @@ namespace ReviewProject.Entities
     {
         public const double DEFAULT_INTEREST_RATE = 0.01;
 
+        private double _interestRate;
+
         public BankAccount(int accountNumber) : this(accountNumber, 0.0)
         {
         }
 
         public BankAccount(int accountNumber, double initialBalance)
         {
+            if (accountNumber <= 0)
+                throw new ArgumentException("Invalid Account Number");
+            if (initialBalance < 0)
+                throw new ArgumentException("Invalid Initial Balance");
             AccountNumber = accountNumber;
             Balance = initialBalance;
             InterestRate = DEFAULT_INTEREST_RATE;
         }
 
-        public int AccountNumber { get; private set;}
+        public int AccountNumber { get; private set; }
 
-        public double Balance { get; private set;}
+        public double Balance { get; private set; }
 
-        public double InterestRate { get; private set;}
+        public double InterestRate
+        {
+            get
+            {
+                return _interestRate;
+            }
+            set
+            {
+                if (value < 0 || value > 0.10)
+                    throw new ArgumentException("Invalid Interest Rate");
+                _interestRate = value;
+            }
+        }
 
         public void Deposit(double amount)
         {
-            throw new NotImplementedException();
+            if (amount <= 0)
+                throw new ArgumentException("Amount must be greater than zero");
+            Balance += amount;
         }
 
         public void Withdraw(double amount)
         {
-            throw new NotImplementedException();
+            if (amount <= 0)
+                throw new ArgumentException("Amount must be greater than zero");
+            if (amount > Balance)
+                throw new ArgumentException("Amount to withdraw exceeds the balance");
+            Balance -= amount;
         }
     }
 }
